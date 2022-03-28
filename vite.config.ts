95a15120397b0a8d1a384ff5170f4d17787a5b0f
Kasper Seweryn <github@wvffle.net>
2022-03-28 19:43:15 +0200

    feat: add auto component import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { dirResolver } from 'vite-auto-import-resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    Vue(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        // 'vue-router',
+        // 'vee-validate',
+        // '@vueuse/head',
+        '@vueuse/core',
+        {
+          // '@vueuse/integrations/useAxios': [ 'useAxios' ],
+          // '@vueuse/integrations/useFuse': [ 'useFuse' ],
+        },
+      ],
+      resolvers: [
+        dirResolver(),
+      ],
+      dts: 'src/auto-imports.d.ts',
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      dts: 'src/components.d.ts',
+    }),
+  ]
 })
